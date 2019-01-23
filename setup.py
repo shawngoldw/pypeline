31993a62ce419ad97cd6338db3cc50b124d52cf7
shawngoldw <shawngoldw@gmail.com>
2019-01-22 22:25:39 -0500

    moved setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
      name='pypeline',
      version='0.1',
@@ -7,6 +10,7 @@ setuptools.setup(
      author="Shawn Goldwasser",
      author_email="shawngoldw@gmail.com",
      description="ML Pipeline Package",
+     long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/shawngoldw/pypeline",
      packages=setuptools.find_packages(),
